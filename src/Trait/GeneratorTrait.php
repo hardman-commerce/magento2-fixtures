fa7b58ad05fdcb4d18ca9da156fd8c75806f18b6
Richard Hardman <richard.hardman@klevu.com>
2025-05-02 14:20:38 +0100

    Enable mocking of Generator using an iterable

---
+++ src/Trait/GeneratorTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © Klevu Oy. All rights reserved. See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace TddWizard\Fixtures\Trait;
+
+trait GeneratorTrait
+{
+    /**
+     * Helper method to convert an array to a generator.
+     * Example usage:
+     *  We have a method we wish to mock that returns a Generator
+     *  $mockClass->method('execute')->willReturn($this->generate(['mockResponse1', 'mockResponse2']));
+     *
+     * @param mixed[]|\Iterator $yieldValues
+     *
+     * @return \Generator
+     */
+    private function generate(array|\Iterator $yieldValues): \Generator
+    {
+        foreach ($yieldValues as $key => $value) {
+            yield $key => $value;
+        }
+    }
+}
